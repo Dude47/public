5a9811aa76a2ad0301d0fd70653b68b9198eef85
Artur S <Dude47@users.noreply.github.com>
2023-08-12 23:23:17 +0300

    Add PersonalAssistantBanking.lua config

---
+++ games/Elder Scrolls Online/live/SavedVariables/PersonalAssistantBanking.lua
@@ -0,0 +1,271 @@
+PersonalAssistantBanking_SavedVariables =
+{
+    ["Default"] = 
+    {
+        ["@Dude_47"] = 
+        {
+            ["$AccountWide"] = 
+            {
+                ["profileCounter"] = 1,
+                [1] = 
+                {
+                    ["transactionWithdrawalStacking"] = 0,
+                    ["name"] = "Profile 1",
+                    ["silentMode"] = true,
+                    ["Custom"] = 
+                    {
+                        ["customItemsEnabled"] = true,
+                        ["PAItemIds"] = 
+                        {
+                            ["30357"] = 
+                            {
+                                ["itemLink"] = "|H0:item:30357:175:1:0:0:0:0:0:0:0:0:0:0:0:0:36:0:0:0:0:0|h|h",
+                                ["operator"] = 1,
+                                ["ruleEnabled"] = true,
+                                ["bagAmount"] = 50,
+                            },
+                        },
+                    },
+                    ["autoExecuteItemTransfers"] = true,
+                    ["Currencies"] = 
+                    {
+                        ["alliancePointsTransaction"] = true,
+                        ["telVarMinToKeep"] = 0,
+                        ["alliancePointsMinToKeep"] = 0,
+                        ["goldMaxToKeep"] = 100000,
+                        ["writVouchersMinToKeep"] = 0,
+                        ["goldMinToKeep"] = 100000,
+                        ["goldTransaction"] = true,
+                        ["telVarTransaction"] = true,
+                        ["currenciesEnabled"] = true,
+                        ["writVouchersTransaction"] = true,
+                        ["telVarMaxToKeep"] = 0,
+                        ["alliancePointsMaxToKeep"] = 0,
+                        ["writVouchersMaxToKeep"] = 0,
+                    },
+                    ["transactionDepositStacking"] = 0,
+                    ["excludeJunk"] = true,
+                    ["Advanced"] = 
+                    {
+                        ["LearnableItemTypes"] = 
+                        {
+                            [8] = 
+                            {
+                                ["Known"] = 1,
+                                ["Unknown"] = 0,
+                            },
+                            [29] = 
+                            {
+                                ["Known"] = 1,
+                                ["Unknown"] = 0,
+                            },
+                        },
+                        ["SpecializedItemTypes"] = 
+                        {
+                            [104] = 1,
+                            [113] = 1,
+                            [108] = 1,
+                            [102] = 1,
+                            [100] = 1,
+                            [109] = 1,
+                            [110] = 1,
+                            [101] = 
+                            {
+                                [24] = 1,
+                                [17] = 1,
+                                [16] = 1,
+                                [13] = 1,
+                                [14] = 1,
+                                [15] = 1,
+                            },
+                        },
+                        ["ItemTypes"] = 
+                        {
+                            [20] = 0,
+                            [21] = 0,
+                            [54] = 0,
+                            [7] = 0,
+                            [26] = 0,
+                            [12] = 0,
+                            [61] = 1,
+                            [30] = 0,
+                            [4] = 0,
+                        },
+                        ["MasterWritCraftingTypes"] = 
+                        {
+                            [1] = 1,
+                            [2] = 1,
+                            [3] = 1,
+                            [4] = 1,
+                            [5] = 1,
+                            [6] = 1,
+                            [7] = 1,
+                        },
+                        ["HolidayWrits"] = 
+                        {
+                            [2760] = 1,
+                        },
+                        ["advancedItemsEnabled"] = true,
+                        ["ItemTraitTypes"] = 
+                        {
+                            [24] = 0,
+                            [9] = 0,
+                            [10] = 0,
+                            [27] = 0,
+                            [20] = 0,
+                            [19] = 0,
+                        },
+                    },
+                    ["AvA"] = 
+                    {
+                        ["CrossAllianceItemIds"] = 
+                        {
+                            [6000] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [2000] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [2100] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [3300] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [5000] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [1100] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [3400] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [2200] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [1000] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [3200] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [3000] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [3100] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [1200] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [1300] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [4000] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                        },
+                        ["ItemIds"] = 
+                        {
+                            [27112] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [27962] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [141731] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [142133] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                            [27138] = 
+                            {
+                                ["operator"] = 0,
+                                ["bagAmount"] = 0,
+                            },
+                        },
+                        ["avaItemsEnabled"] = false,
+                    },
+                    ["Crafting"] = 
+                    {
+                        ["ItemTypes"] = 
+                        {
+                            [64] = 1,
+                            [65] = 1,
+                            [66] = 1,
+                            [35] = 1,
+                            [36] = 1,
+                            [37] = 1,
+                            [38] = 1,
+                            [39] = 1,
+                            [40] = 1,
+                            [41] = 1,
+                            [42] = 1,
+                            [43] = 1,
+                            [44] = 1,
+                            [45] = 1,
+                            [46] = 1,
+                            [16] = 1,
+                            [17] = 1,
+                            [51] = 1,
+                            [52] = 1,
+                            [53] = 1,
+                            [63] = 1,
+                            [68] = 1,
+                            [58] = 1,
+                            [67] = 1,
+                            [33] = 1,
+                            [10] = 1,
+                            [62] = 1,
+                            [31] = 1,
+                        },
+                        ["craftingItemsEnabled"] = true,
+                    },
+                    ["autoStackBags"] = true,
+                },
+                ["savedVarsVersion"] = 20230728,
+                ["version"] = 2,
+            },
+        },
+    },
+}
